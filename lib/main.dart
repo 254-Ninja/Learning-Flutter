4ab640c859a5897b31977fb52b7efcbeaeed020d
254-Ninja <you@example.com>
2020-02-29 19:25:31 +0300

    profile

--- lib/main.dart
+++ lib/main.dart
@@ -29,11 +29,20 @@ class HomeScreen extends StatelessWidget{
                   Colors.orangeAccent,
                 ])
               ),
-              child: Text('This is drawer header')),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Material(
+                      child: Image.asset('images/test.jpg',width: 100,height: 100,),
+                    )
+                  ],
+                ),
+              )),
           CustomListTile(Icons.person,'Profile',()=>{}),
-          CustomListTile(Icons.person,'Profile',()=>{}),
-          CustomListTile(Icons.person,'Profile',()=>{}),
-          CustomListTile(Icons.person,'Profile',()=>{}),
+          CustomListTile(Icons.notifications,'My Trips',()=>{}),
+          CustomListTile(Icons.map,'Available Route',()=>{}),
+          CustomListTile(Icons.settings,'Settings',()=>{}),
+          CustomListTile(Icons.lock,'Log Out',()=>{}),
 
         ],
       ),
@@ -55,28 +64,33 @@ class CustomListTile extends StatelessWidget{
     // TODO: implement build
     return Padding(
       padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-      child: InkWell(
-        splashColor: Colors.orangeAccent,
-        onTap: onTap,
-        child: Container(
-          height: 40,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Icon(icon),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(text,style: TextStyle(
-                      fontSize: 16.0
-                    ),),
-                  ),
-                ],
-              ),
-              Icon(Icons.arrow_right)
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey.shade400))
+        ),
+        child: InkWell(
+          splashColor: Colors.orangeAccent,
+          onTap: onTap,
+          child: Container(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(icon),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(text,style: TextStyle(
+                        fontSize: 16.0
+                      ),),
+                    ),
+                  ],
+                ),
+                Icon(Icons.arrow_right)
 
-            ],
+              ],
+            ),
           ),
         ),
       ),
